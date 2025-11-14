e4ed50aeb2ce135b3d5649873fd028f20c759c49
Aaron Parandian <aparandian@apple.com>
2025-11-13 16:23:37 -0800

    12. add product, service and vendor in data and web layer

--- internal/database/client.go
+++ internal/database/client.go
@@ -14,11 +14,16 @@ import (
 
 type DatabaseClient interface {
 	Ready() bool
+
 	GetAllCustomers(ctx context.Context, emailAddress string) ([]models.Customer, error)
 	GetAllProducts(ctx context.Context, vendor_id string) ([]models.Product, error)
 	GetAllServices(ctx context.Context) ([]models.Service, error)
 	GetAllVendors(ctx context.Context) ([]models.Vendor, error)
+
 	AddCustomer(ctx context.Context, customer *models.Customer) (*models.Customer, error)
+	AddProduct(ctx context.Context, product *models.Product) (*models.Product, error)
+	AddVendor(ctx context.Context, vendor *models.Vendor) (*models.Vendor, error)
+	AddService(ctx context.Context, service *models.Service) (*models.Service, error)
 }
 
 type Client struct {
